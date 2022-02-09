f694c710071467a10c53c61a239775ab1cd68d6d
jabaracconich <87547340+jabaracconich@users.noreply.github.com>
2022-02-09 17:15:52 -0500

    a

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,30 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  console.log("logger middleware")
+
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  console.log("validateUser middleware")
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  console.log("validateUser middleware")
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  console.log("validatePost middleware")
 }
 
 // do not forget to expose these functions to other modules
+
+
+module.exports = {
+    logger,
+    validateUserId,
+    validateUser,
+    validatePost
+}
